39f7e1b39c5dc1c13950d793c25a1a8fecda794c
Stanciu, Mihai <doru-mihai.stanciu@anritsu.com>
2021-09-22 21:14:11 +0300

    apply patch based on:  line 356 - websocket/atmosphere support  line 462 - spring framework issuue

--- dev/core/src/com/google/gwt/dev/shell/jetty/JettyLauncher.java
+++ dev/core/src/com/google/gwt/dev/shell/jetty/JettyLauncher.java
@@ -353,6 +353,7 @@ public class JettyLauncher extends ServletContainerLauncher {
       private final ClasspathPattern systemClassesFromWebappFirst = new ClasspathPattern(new String[] {
           "-javax.servlet.",
           "-javax.el.",
+          "-javax.websocket.",
           "javax.",
       });
       private final ClasspathPattern allowedFromSystemClassLoader = new ClasspathPattern(new String[] {
@@ -458,7 +459,7 @@ public class JettyLauncher extends ServletContainerLauncher {
         String resourceName = name.replace('.', '/') + ".class";
         URL found = systemClassLoader.getResource(resourceName);
         if (found == null) {
-          return null;
+          throw new ClassNotFoundException(name);
         }
 
         // Special-case JDBCUnloader; it should always be loaded in the webapp classloader
