e9e26e1be74957cc19eebcf3a376829c3d378d49
Colin Alworth <niloc132@gmail.com>
2022-07-07 14:38:45 -0500

    break some things

--- dev/core/super/com/google/gwt/core/client/GwtScriptOnly.java
+++ dev/core/super/com/google/gwt/core/client/GwtScriptOnly.java
@@ -7,7 +7,8 @@
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by
+ * applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
@@ -16,8 +17,9 @@
 package com.google.gwt.core.client;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
+
+import java.lang.annotation.ElementType;
 
 /**
  * This annotation is used to break out of a module's source path in hosted
@@ -41,4 +43,9 @@ import java.lang.annotation.Target;
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Deprecated
 public @interface GwtScriptOnly {
+
+
+
 }
+
+
