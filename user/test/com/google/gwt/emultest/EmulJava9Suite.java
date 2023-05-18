c7f737a1ff428634472d9a550eee3c53df8550e4
Colin Alworth <colin@vertispan.com>
2023-05-18 10:50:29 -0500

    Correct licenses, run all tests in suites

--- user/test/com/google/gwt/emultest/EmulJava9Suite.java
+++ user/test/com/google/gwt/emultest/EmulJava9Suite.java
@@ -15,8 +15,17 @@
  */
 package com.google.gwt.emultest;
 
+import com.google.gwt.emultest.java9.util.stream.CollectorsTest;
+import com.google.gwt.emultest.java9.util.stream.DoubleStreamTest;
+import com.google.gwt.emultest.java9.util.stream.IntStreamTest;
+import com.google.gwt.emultest.java9.util.stream.LongStreamTest;
+import com.google.gwt.emultest.java9.util.stream.StreamTest;
 import com.google.gwt.emultest.java9.util.ListTest;
 import com.google.gwt.emultest.java9.util.MapTest;
+import com.google.gwt.emultest.java9.util.OptionalDoubleTest;
+import com.google.gwt.emultest.java9.util.OptionalIntTest;
+import com.google.gwt.emultest.java9.util.OptionalLongTest;
+import com.google.gwt.emultest.java9.util.OptionalTest;
 import com.google.gwt.emultest.java9.util.SetTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -25,9 +34,18 @@ import org.junit.runners.Suite.SuiteClasses;
 /** Test JRE emulations. */
 @RunWith(Suite.class)
 @SuiteClasses({
+        CollectorsTest.class,
+        DoubleStreamTest.class,
+        IntStreamTest.class,
+        LongStreamTest.class,
+        StreamTest.class,
         ListTest.class,
-        SetTest.class,
-        MapTest.class
+        MapTest.class,
+        OptionalDoubleTest.class,
+        OptionalIntTest.class,
+        OptionalLongTest.class,
+        OptionalTest.class,
+        SetTest.class
 })
 public class EmulJava9Suite {
 }
