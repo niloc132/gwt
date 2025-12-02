cb1704141cdc2dd9b91630566c8a4ab6eaaf8a4c
Colin Alworth <colin@vertispan.com>
2025-12-02 11:02:46 -0600

    Fix comments, copyright header

--- user/test/com/google/gwt/user/rebind/rpc/testcases/client/RecursiveTypeGraphInstantiability.java
+++ user/test/com/google/gwt/user/rebind/rpc/testcases/client/RecursiveTypeGraphInstantiability.java
@@ -1,19 +1,37 @@
+/*
+ * Copyright 2025 GWT Project Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.google.gwt.user.rebind.rpc.testcases.client;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
+/**
+ * Test case for 10181, where it is possible to end up with a TIC with apparently no instantiable
+ * subtypes.
+ */
 public interface RecursiveTypeGraphInstantiability {
   /**
-   * Not serializable; interface only
+   * Base interface, used as the root type.
    */
   interface A extends IsSerializable {
   }
 
   /**
-   * Auto serializable, but with back-reference to A for which the question of instantiable subtypes
-   * depends on the serializability of this class. The reference to {@link C} which
-   * has a back-reference to {@link B} and B being its only subtypes candidate
-   * helps reproduce issue 10181.
+   * Implements A, and references C, which also has a field of type A. This cycle apparently causes
+   * A to be marked as "done" before its subtypes have been completely checked, and stops later
+   * checks from taking place.
    */
   class B implements A {
     private B b;
@@ -21,12 +39,8 @@ public interface RecursiveTypeGraphInstantiability {
   }
 
   /**
-   * Auto-serializable with back-reference to B through D, a different interface, triggering another
-   * descent through checkSubtypes, finding an already "done" TIC for {@link B} with
-   * {@code instantiable==false}. Being "done", it is used to decide instantiability of the only
-   * subclass candidate of {@link D}, which is {@link B}, resulting in {@code false} for
-   * the instantiability of {@link D}, and thus of {@link C}, and thus of {@link B}, causing
-   * the test case to fail prior to a fix for issue 10181.
+   * Independent serializable type, reachable from B, but references A. This causes A to be re-checked
+   * while C is processed, which leads to A being marked as having no subtypes.
    */
   class C implements IsSerializable {
     private A a;
