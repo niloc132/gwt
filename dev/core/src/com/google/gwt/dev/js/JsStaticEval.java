cea6101317d61cc1d3d616d7a48e0dff222aeb1b
Colin Alworth <colin@vertispan.com>
2026-09-09 20:16:07 -0500

    Reduce scope slightly, rewrite postfix to prefix, and add tests

--- dev/core/src/com/google/gwt/dev/js/JsStaticEval.java
+++ dev/core/src/com/google/gwt/dev/js/JsStaticEval.java
@@ -716,17 +716,23 @@ public class JsStaticEval {
       return arg1;
     }
 
-    // Likewise, simplify (name++, name) to (name++)
+    // Likewise, simplify (name++, name) or (++name, name) to (++name)
     if (arg1 instanceof JsUnaryOperation unaryOp
         && arg2 instanceof JsNameRef resultRef
-        && unaryOp.getOperator().isModifying()
-        && unaryOp.getArg() instanceof JsNameRef assignRef
+        && (unaryOp.getOperator() == JsUnaryOperator.INC
+            || unaryOp.getOperator() == JsUnaryOperator.DEC)
+        && unaryOp.getArg() instanceof JsNameRef unaryArgRef
         && resultRef.getQualifier() == null
-        && assignRef.getQualifier() == null
-        && assignRef.getName() == resultRef.getName()) {
-      assert assignRef.isResolved() : "assignRef was not resolved: " + assignRef;
+        && unaryArgRef.getQualifier() == null
+        && unaryArgRef.getName() == resultRef.getName()) {
+      assert unaryArgRef.isResolved() : "unaryArgRef was not resolved: " + unaryArgRef;
       assert resultRef.isResolved() : "resultRef was not resolved: " + resultRef;
-      return arg1;
+      if (unaryOp instanceof JsPrefixOperation) {
+        return unaryOp;
+      }
+      // Rewrite the discarded postfix operation into the equivalent prefix operation.
+      return new JsPrefixOperation(unaryOp.getSourceInfo(), unaryOp.getOperator(),
+          unaryOp.getArg());
     }
 
     return expr;
